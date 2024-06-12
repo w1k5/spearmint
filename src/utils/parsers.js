8ff2bd38dfa91598a13eab9f763abf1c2223998d
wiks <mvictoria00@outlook.com>
2024-06-12 16:16:32 -0400

    set up gh pages

--- src/utils/parsers.js
+++ src/utils/parsers.js
@@ -1,24 +1,26 @@
 // parsers.js
 export const parseData = (data) => {
-    return data.map(item => {
-        try {
-            const date = new Date(item.Date || item.date);
-            const amount = parseFloat(item.Amount || item.amount);
+  return data
+    .map((item) => {
+      try {
+        const date = new Date(item.Date || item.date)
+        const amount = parseFloat(item.Amount || item.amount)
 
-            if (isNaN(date.getTime()) || isNaN(amount)) {
-                console.error(`Invalid data entry: ${JSON.stringify(item)}`);
-                return null;
-            }
-
-            return {
-                date: date.toISOString().split('T')[0], // Format as 'YYYY-MM-DD'
-                amount,
-                category: item.Category || item.category,
-                description: item.Description || item.description,
-            };
-        } catch (error) {
-            console.error(`Data parsing error: ${error.message}`, item);
-            return null;
+        if (isNaN(date.getTime()) || isNaN(amount)) {
+          console.error(`Invalid data entry: ${JSON.stringify(item)}`)
+          return null
         }
-    }).filter(entry => entry !== null); // Filter out invalid entries
-};
+
+        return {
+          date: date.toISOString().split('T')[0], // Format as 'YYYY-MM-DD'
+          amount,
+          category: item.Category || item.category,
+          description: item.Description || item.description
+        }
+      } catch (error) {
+        console.error(`Data parsing error: ${error.message}`, item)
+        return null
+      }
+    })
+    .filter((entry) => entry !== null) // Filter out invalid entries
+}
