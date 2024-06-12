31b113ece018ec283ed8848df7d3fc54d41cfe78
wiks <mvictoria00@outlook.com>
2024-06-12 16:44:44 -0400

    change router type

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 // App.js
-import React, { useState } from 'react';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import React from 'react';
+import {HashRouter, Route, Routes} from 'react-router-dom';
 import Home from './pages/Home';
-import {parseData} from "./utils/parsers";
 
 const App = () => {
-    const [data, setData] = useState([]);
-
-    const handleFileUploaded = (fileData) => {
-        const parsedData = parseData(fileData);
-        setData(parsedData);
-    };
-
     return (
-        <Router>
+        <HashRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
             </Routes>
-        </Router>
+        </HashRouter>
     );
 };
 
