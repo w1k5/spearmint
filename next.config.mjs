dd858246756f63236988ba913d9e61a33885930d
w1k5 <vmichalska00@gmail.com>
2024-07-18 19:36:07 -0400

    fix gh-pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
+    assetPrefix: isProd ? './' : '',
     output: 'export', // Outputs a Single-Page Application (SPA).
     distDir: './build', // Changes the build output directory to `./dist/`.
 }
