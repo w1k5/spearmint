b1c2de20c351748f887db586f2f86adabb3df265
w1k5 <w1k5@users.noreply.github.com>
2024-08-04 02:30:05 +0000

    deploy: 8a1343cf121eabe22875755fedbe2c4f260c3f4a

---
+++ _next/static/chunks/pages/index-86f8839614ad67e0.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{5557:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return _(1852)}])},1852:function(n,u,_){"use strict";_.r(u);var t=_(5893);_(7294);var e=_(4710);u.default=()=>(0,t.jsx)(e.default,{})}},function(n){n.O(0,[251,196,378,385,888,774,179],function(){return n(n.s=5557)}),_N_E=n.O()}]);
