3ce1c9846c913039308304ac8ae3f89611958e38
w1k5 <vmichalska00@gmail.com>
2024-08-12 18:27:19 -0400

    add footer and fix favicon

---
+++ src/components/headers_footers/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from '../../styles/Footer.module.css';
+
+const Footer = () => {
+    return (
+        <footer className={styles.footer}>
+            <p>© {new Date().getFullYear()} w1k5. All rights reserved.</p>
+            <p>Made with ❤️ by Victoria Michalska</p>
+        </footer>
+    );
+};
+
+export default Footer;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,23 +2,25 @@
 import React from 'react';
 import './index.css';
 import './dashboard.css';
-import Header from "../components/header/Header";
+import Header from "../components/headers_footers/Header";
 import Head from "next/head";
+import Footer from "../components/headers_footers/Footer";
 
 const App = ({ Component, pageProps }) => {
     return (
         <React.StrictMode>
             <Head>
-                <link rel="apple-touch-icon" sizes="180x180" href="/favicon_io/apple-touch-icon.png"/>
-                <link rel="icon" type="image/png" sizes="32x32" href="/favicon_io/favicon-32x32.png"/>
-                <link rel="icon" type="image/png" sizes="16x16" href="/favicon_io/favicon-16x16.png"/>
-                <link rel="manifest" href="/favicon_io/site.webmanifest"/>
+                <link rel="apple-touch-icon" sizes="180x180" href="/src/assets/favicon_io/apple-touch-icon.png"/>
+                <link rel="icon" type="image/png" sizes="32x32" href="/src/assets/favicon_io/favicon-32x32.png"/>
+                <link rel="icon" type="image/png" sizes="16x16" href="/src/assets/favicon_io/favicon-16x16.png"/>
+                <link rel="manifest" href="../assets/favicon_io/site.webmanifest"/>
                 <meta name="description" content="Analyze your spending!"/>
                 <meta name="keywords" content="React, dashboard, finance, expenses, income"/>
                 <title>Spearmint Budgeting</title>
             </Head>
             <Header/>
             <Component {...pageProps} />
+            <Footer />
         </React.StrictMode>
     );
 };
