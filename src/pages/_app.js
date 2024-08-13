dfdcb0f02cb1f8eb2bc023c50195f9fe8087151e
w1k5 <vmichalska00@gmail.com>
2024-08-12 22:25:23 -0400

    favicon hardcoded D:

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -13,10 +13,10 @@ const App = ({ Component, pageProps }) => {
     return (
         <React.StrictMode>
             <Head>
-                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-32x32.png"/>
-                <link rel="manifest" href="/site.webmanifest"/>
+                <link rel="apple-touch-icon" sizes="180x180" href="/spearmint/apple-touch-icon.png"/>
+                <link rel="icon" type="image/png" sizes="32x32" href="/spearmint/favicon-32x32.png"/>
+                <link rel="icon" type="image/png" sizes="16x16" href="/spearmint/favicon-32x32.png"/>
+                <link rel="manifest" href="/spearmint/site.webmanifest"/>
                 <meta name="description" content="Analyze your spending!"/>
                 <meta name="keywords" content="React, dashboard, finance, expenses, income"/>
                 <title>Spearmint Budgeting</title>
