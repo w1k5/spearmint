473ca735519980c941ca082ef83e5bcc8c8c2974
w1k5 <vmichalska00@gmail.com>
2024-08-12 20:57:55 -0400

    fix broken url

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,13 +10,17 @@ const App = ({ Component, pageProps }) => {
     return (
         <React.StrictMode>
             <Head>
+                <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon_io/apple-touch-icon.png"/>
+                <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon_io/favicon-32x32.png"/>
+                <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon_io/favicon-32x32.png"/>
+                <link rel="manifest" href="/assets/favicon_io/site.webmanifest"/>
                 <meta name="description" content="Analyze your spending!"/>
                 <meta name="keywords" content="React, dashboard, finance, expenses, income"/>
                 <title>Spearmint Budgeting</title>
             </Head>
             <Header/>
             <Component {...pageProps} />
-            <Footer />
+            <Footer/>
         </React.StrictMode>
     );
 };
